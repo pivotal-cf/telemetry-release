bf6db85e46842e679d735edb93b3eda6f4a70ef7
Derek Richard <drichard@pivotal.io>
2019-04-02 17:42:32 -0400

    Fix path to bosh cli binary in create release script
    - need permissions to run binary
    
    [#165028202]
    
    Co-authored-by: An Yu <anyu@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- ci/tasks/create-release.sh
+++ ci/tasks/create-release.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 TASK_DIR="$PWD"
-BOSH_CLI="$PWD/bosh-cli-github-release/bosh-cli-*-linux-amd64"
+BOSH_CLI="$PWD"/bosh-cli-github-release/bosh-cli-*-linux-amd64
+chmod 755 "$BOSH_CLI"
 
 cd telemetry-components-release
 "$BOSH_CLI" create-release --force --tarball "$TASK_DIR/release-tarball/release.tgz"
