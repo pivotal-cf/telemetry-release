8dec718088da185601a3b912171bd004615f171e
Derek Richard <drichard@pivotal.io>
2019-04-03 09:55:14 -0400

    Ensure git dependency is installed for create release job
    [#165028202]
    
    Co-authored-by: An Yu <anyu@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- ci/tasks/create-release.sh
+++ ci/tasks/create-release.sh
@@ -6,5 +6,7 @@ TASK_DIR="$PWD"
 BOSH_CLI=("$PWD"/bosh-cli-github-release/bosh-cli-*-linux-amd64)
 chmod 755 "$BOSH_CLI"
 
-cd telemetry-components-release
+apt-get update
+apt-get -y install git
+pushd telemetry-components-release
 "$BOSH_CLI" create-release --force --tarball "$TASK_DIR/release-tarball/release.tgz"
