f821d8ddc4de1935377b6845fb6f8fc9cd869c94
Derek Richard <drichard@pivotal.io>
2019-04-09 16:03:59 -0400

    Strip whitespace from expected output msg from acceptance test
    [#165184617 ]
    
    Co-authored-by: An Yu <anyu@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- ci/tasks/test-acceptance-sends-to-centralizer.sh
+++ ci/tasks/test-acceptance-sends-to-centralizer.sh
@@ -23,8 +23,7 @@ assert_centralizer_log() {
 
 echo Testing that logs matching telemetry-source embedded as a string value in a JSON object are extracted and sent to centralizer
 INPUT_LOG='{ "time": 12341234123412, "level": "info", "message": "{ \"data\": {\"app\": \"da\\\"ta\", \"counter\": 42}, \"telemetry-source\": \"my-origin\"}'
-EXPECTED_LOG='{ "data": {"app": "da\"ta", "counter": 42}, "telemetry-source": "my-origin"}'
-INSERT_AGENT_LOG_CMD="echo \"$INPUT_LOG\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
+EXPECTED_LOG='{"data":{"app":"da\"ta","counter":42},"telemetry-source":"my-origin"}'
 ASSERT_CENTRALIZER_LOG_CMD="sudo grep \"$EXPECTED_LOG\" /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log"
 assert_centralizer_log "$EXPECTED_LOG" "$ASSERT_CENTRALIZER_LOG_CMD"
 
