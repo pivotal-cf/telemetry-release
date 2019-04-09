1f344f38d0a8ca78bf9a6634cf5579c0f02631c4
Derek Richard <drichard@pivotal.io>
2019-04-09 15:50:06 -0400

    Update acceptance test assertions with expected structure
    - tests now test msgs embedded as string in JSON object
    
    [#165184617]
    
    Co-authored-by: An Yu <anyu@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- ci/tasks/test-acceptance-sends-to-centralizer.sh
+++ ci/tasks/test-acceptance-sends-to-centralizer.sh
@@ -14,21 +14,21 @@ chmod 755 "$BBL_CLI"
 cd bbl-state
 eval "$("$BBL_CLI" print-env)"
 
-echo Testing that logs matching telemetry-source are sent to centralizer
-EXPECTED_LOG="this is a telemetry-source test at $(date +%s)"
-INSERT_AGENT_LOG_CMD="echo \"$EXPECTED_LOG\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
+assert_centralizer_log() {
+  INSERT_AGENT_LOG_CMD="echo \"$1\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
+  "$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-agent -c "$INSERT_AGENT_LOG_CMD"
+  sleep 5
+  "$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-centralizer -c "$2"
+}
+
+echo Testing that logs matching telemetry-source embedded as a string value in a JSON object are extracted and sent to centralizer
+INPUT_LOG='{ "time": 12341234123412, "level": "info", "message": "{ \"data\": {\"app\": \"da\\\"ta\", \"counter\": 42}, \"telemetry-source\": \"my-origin\"}'
+EXPECTED_LOG='{ "data": {"app": "da\"ta", "counter": 42}, "telemetry-source": "my-origin"}'
+INSERT_AGENT_LOG_CMD="echo \"$INPUT_LOG\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
 ASSERT_CENTRALIZER_LOG_CMD="sudo grep \"$EXPECTED_LOG\" /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log"
+assert_centralizer_log "$EXPECTED_LOG" "$ASSERT_CENTRALIZER_LOG_CMD"
 
-"$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-agent -c "$INSERT_AGENT_LOG_CMD"
-sleep 5
-"$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-centralizer -c "$ASSERT_CENTRALIZER_LOG_CMD"
-
-
-echo Testing that logs not matching telemetry-source are not sent to centralizer
-EXPECTED_LOG="NOT a telemetry msg test at $(date +%s)"
-INSERT_AGENT_LOG_CMD="echo \"$EXPECTED_LOG\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
+echo Testing that logs not matching the expected structure are filtered out by the centralizer
+EXPECTED_LOG="NOT a telemetry-source msg test at $(date +%s)"
 ASSERT_CENTRALIZER_LOG_CMD="if sudo grep \"$EXPECTED_LOG\" /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log; then exit 1; fi"
-
-"$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-agent -c "$INSERT_AGENT_LOG_CMD"
-sleep 5
-"$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-centralizer -c "$ASSERT_CENTRALIZER_LOG_CMD"
+assert_centralizer_log "$EXPECTED_LOG" "$ASSERT_CENTRALIZER_LOG_CMD"
