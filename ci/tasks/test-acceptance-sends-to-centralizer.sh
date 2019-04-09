060504b8fccb23f3e8eb6992d0d264acaac419fc
Derek Richard <drichard@pivotal.io>
2019-04-09 16:31:48 -0400

    Fixing quotes, maybe
    [#165184617]
    
    Co-authored-by: An Yu <anyu@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- ci/tasks/test-acceptance-sends-to-centralizer.sh
+++ ci/tasks/test-acceptance-sends-to-centralizer.sh
@@ -15,7 +15,7 @@ cd bbl-state
 eval "$("$BBL_CLI" print-env)"
 
 assert_centralizer_log() {
-  INSERT_AGENT_LOG_CMD="echo \"$1\" | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
+  INSERT_AGENT_LOG_CMD="echo '$1' | sudo tee -a /var/vcap/sys/log/telemetry-agent/telemetry-agent.stdout.log"
   "$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-agent -c "$INSERT_AGENT_LOG_CMD"
   sleep 5
   "$BOSH_CLI" -d telemetry-components-acceptance ssh telemetry-centralizer -c "$2"
@@ -24,10 +24,10 @@ assert_centralizer_log() {
 echo Testing that logs matching telemetry-source embedded as a string value in a JSON object are extracted and sent to centralizer
 INPUT_LOG='{ "time": 12341234123412, "level": "info", "message": "{ \"data\": {\"app\": \"da\\\"ta\", \"counter\": 42}, \"telemetry-source\": \"my-origin\"}'
 EXPECTED_LOG='{"data":{"app":"da\"ta","counter":42},"telemetry-source":"my-origin"}'
-ASSERT_CENTRALIZER_LOG_CMD="sudo grep \"$EXPECTED_LOG\" /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log"
+ASSERT_CENTRALIZER_LOG_CMD="sudo grep '$EXPECTED_LOG' /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log"
 assert_centralizer_log "$INPUT_LOG" "$ASSERT_CENTRALIZER_LOG_CMD"
 
 echo Testing that logs not matching the expected structure are filtered out by the centralizer
 INPUT_LOG="NOT a telemetry-source msg test at $(date +%s)"
-ASSERT_CENTRALIZER_LOG_CMD="if sudo grep \"$INPUT_LOG\" /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log; then exit 1; fi"
+ASSERT_CENTRALIZER_LOG_CMD="if sudo grep '$INPUT_LOG' /var/vcap/sys/log/telemetry-centralizer/telemetry-centralizer.stdout.log; then exit 1; fi"
 assert_centralizer_log "$INPUT_LOG" "$ASSERT_CENTRALIZER_LOG_CMD"
