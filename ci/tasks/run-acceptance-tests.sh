f470a53a3222ecbe153f64ea26238c95e5d7c7b2
Derek Richard <drichard@pivotal.io>
2020-01-09 12:16:28 -0500

    Ensure correct version of bundler is installed
    [#169851734]

--- ci/tasks/run-acceptance-tests.sh
+++ ci/tasks/run-acceptance-tests.sh
@@ -3,5 +3,6 @@
 base="$(dirname "$BASH_SOURCE[0]")/../.."
 
 cd "$base/src/acceptance_tests"
+gem install bundler:1.16.2
 bundle
 rspec spec
--- ci/tasks/run-centralizer-unit-tests.sh
+++ ci/tasks/run-centralizer-unit-tests.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 cd telemetry-release/src/fluentd/telemetry-filter-plugin
+gem install bundler:1.16.2
 bundle && rspec .
