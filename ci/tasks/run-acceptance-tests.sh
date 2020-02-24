19c09fa8040dc2f2099faa1f3c2b7f68e0ad9dfe
Yechiel Kalmenson <ykalmenson@pivotal.io>
2020-02-24 11:54:16 -0500

    fix broken test scripts
    Co-authored-by: Sanhita Mukherji <smukherji@pivotal.io>

--- ci/tasks/run-acceptance-tests.sh
+++ ci/tasks/run-acceptance-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 base="$(dirname "$BASH_SOURCE[0]")/../.."
-AUDIT_MODE="${$1-false}"
-if [AUDIT_MODE != "true"]; then
+AUDIT_MODE="${1-false}"
+if [ "$AUDIT_MODE" != "true" ]; then
 cd "$base/src/acceptance_tests"
 else cd "$base/src/acceptance_tests_audit_mode"
 fi
