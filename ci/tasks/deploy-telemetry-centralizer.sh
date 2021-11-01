199b1b0044d5c0e5708aab1efcbf97984f83d310
Matt Campbell <macampbell@vmware.com>
2021-11-01 15:51:00 -0400

    Remove force flag as it does not exist for upload release

--- ci/tasks/deploy-telemetry-centralizer.sh
+++ ci/tasks/deploy-telemetry-centralizer.sh
@@ -49,8 +49,8 @@ echo "Uploading stemcell..."
 retry 5 "$BOSH_CLI" upload-stemcell "$TASK_DIR/xenial-stemcell/stemcell.tgz"
 
 echo "Uploading releases..."
-retry 5 "$BOSH_CLI" upload-release --force "$TASK_DIR/release-tarball/release.tgz"
-retry 5 "$BOSH_CLI" upload-release --force "$TASK_DIR/bpm-release/release.tgz"
+retry 5 "$BOSH_CLI" upload-release "$TASK_DIR/release-tarball/release.tgz"
+retry 5 "$BOSH_CLI" upload-release "$TASK_DIR/bpm-release/release.tgz"
 
 echo "Deploying telemetry centralizer"
 retry 5 "$BOSH_CLI" deploy -d "$DEPLOYMENT_NAME" "$TASK_DIR/telemetry-release/manifest/centralizer.yml" \
