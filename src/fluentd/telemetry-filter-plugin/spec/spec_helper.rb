647c8032f485705f4aec4dff9ffee309092fa5fc
David Riddle <driddle@vmware.com>
2023-08-29 09:47:45 -0500

    Remove telemetry-reciever-app code from xenial branch

--- src/fluentd/telemetry-filter-plugin/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
-require 'rspec'
-require "fluent/test"
-require "fluent/test/helpers"
-require "fluent/test/driver/filter"
-
-# prevent Test::Unit's AutoRunner from executing
-Test::Unit.run = true
