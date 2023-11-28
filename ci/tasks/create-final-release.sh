1c7dbb8483b84336ede6d73f0c04206ff57c7e35
David Riddle <driddle@vmware.com>
2023-11-28 10:56:54 -0600

    Only create a single version of the release (same code between jammy and xenial)

--- ci/tasks/create-final-release.sh
+++ ci/tasks/create-final-release.sh
@@ -21,7 +21,7 @@ blobstore:
       $(echo $GCS_SERVICE_ACCOUNT_KEY)
 EOM
 
-  "$BOSH_CLI" create-release --final --version "${VERSION}-${STEMCELL_VERSION}"
+  "$BOSH_CLI" create-release --final --version "${VERSION}"
 
   git add .
   git config --global user.name $GITHUB_NAME
