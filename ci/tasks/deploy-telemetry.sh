26531c282c38662f3512ffd349b5367905ba7913
David Riddle <driddle@vmware.com>
2024-07-19 14:05:45 -0500

    Fix jq snippet for grabbing AZs

--- ci/tasks/deploy-telemetry.sh
+++ ci/tasks/deploy-telemetry.sh
@@ -74,7 +74,7 @@ echo "BOSH_ENVIRONMENT: $BOSH_ENVIRONMENT"
 eval $(smith om -l testbed-lease/metadata)
 
 export NETWORK=$(smith read --lockfile=testbed-lease/metadata | jq -r .ert_subnet)
-export AZ=$(smith read --lockfile=testbed-lease/metadata | jq -r .azs[0])
+export AZ=$(smith read --lockfile=testbed-lease/metadata | jq -r '.azs[0]')
 
 echo "Uploading stemcell..."
 
